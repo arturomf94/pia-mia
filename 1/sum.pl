04e3fde12b5fdaaaae44a8780246fb05a2a84eac
arturomf94 <arturomf94@gmail.com>
2018-09-19 22:38:25 -0500

    add prolog with recursive and tail recursive sum

---
+++ 1/sum.pl
@@ -0,0 +1,20 @@
+:- use_module(library(statistics)).
+
+sum_list([],0).
+
+sum_list([Top|Tail], Total) :-
+	sum_list(Tail, TotalRest), 
+	Total is Top + TotalRest.
+
+sum_listTR([],X,X).
+
+sum_listTR([Top|Tail], Sum, Total) :- 
+	TotalNow is Top + Sum,
+	sum_listTR(Tail,TotalNow, Total).
+
+nlist(0,[]) .
+nlist(N,[N|Ns]) :- N1 is N - 1, nlist(N1, Ns).
+
+sum1(L,R) :- nlist(L,X), sum_list(X,R).
+
+sum2(L,R) :- nlist(L,X), sum_listTR(X,0,R).
