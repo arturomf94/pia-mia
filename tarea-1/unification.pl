8ab046d38ca89b29532960bbf28b3f02c5c0391d
arturomf94 <arturomf94@gmail.com>
2018-10-12 12:07:46 -0500

    unify variables!

--- tarea-1/unification.pl
+++ tarea-1/unification.pl
@@ -3,13 +3,22 @@
 unify(T1, T2) :-
     atomic(T1),
     atomic(T2),
-    T1 == T2.
+    T1 == T2,
+    !.
 
 unify(T1, T2) :-
     var(T1),
-    T1 == T2.
+    T1 == T2,
+    !.
 
 unify(T1, T2) :-
     var(T1),
     \+ (T1 == T2),
-    write(T1), write(' = '), write(T2).
+    T1 = T2,
+    !.
+
+unify(T1, T2) :-
+    var(T2),
+    \+ (T1 == T2),
+    T2 = T1,
+    !.
