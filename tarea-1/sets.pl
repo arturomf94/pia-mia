4b46dfea71e47316b7502f29e7bb0006717d850c
arturomf94 <arturomf94@gmail.com>
2018-10-14 21:47:44 -0500

    add more set operations

--- tarea-1/sets.pl
+++ tarea-1/sets.pl
@@ -27,3 +27,44 @@ inter_aux([Head|Tail], L2, R, Acc) :-
     not(member(Head,L2)),
     inter_aux(Tail, L2, R, Acc),
     !.
+
+union([], L2, L2) :- !.
+
+union(L1, [], L1) :- !.
+
+union(L1, L2, R) :-
+    union_aux(L1, L2, R, L2).
+
+union_aux([], _, Acc, Acc) :- !.
+
+union_aux([Head|Tail], L2, R, Acc) :-
+    not(member(Head, L2)),
+    append([Head], Acc, AccNew),
+    union_aux(Tail, L2, R, AccNew),
+    !.
+
+union_aux([Head|Tail], L2, R, Acc) :-
+    member(Head, L2),
+    union_aux(Tail, L2, R, Acc),
+    !.
+
+dif([], _L2, []) :- !.
+
+dif(L1, [], L1) :- !.
+
+dif(L1, L2, R) :-
+    inter(L1, L2, NewL2),
+    dif_aux(L1, NewL2, R, []).
+
+dif_aux([], _, Acc, Acc) :- !.
+
+dif_aux([Head|Tail], L2, R, Acc) :-
+    not(member(Head, L2)),
+    append([Head], Acc, AccNew),
+    dif_aux(Tail, L2, R, AccNew),
+    !.
+
+dif_aux([Head|Tail], L2, R, Acc) :-
+    member(Head, L2),
+    dif_aux(Tail, L2, R, Acc),
+    !.
