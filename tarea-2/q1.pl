ab9616675aeeabb304f2b23d170e796bb6ba07d3
arturomf94 <arturomf94@gmail.com>
2018-10-24 23:43:43 -0500

    finally stopped moving animals around

--- tarea-2/q1.pl
+++ tarea-2/q1.pl
@@ -2,13 +2,37 @@
 odia(perro, gato).
 odia(gato, hamster).
 
-s([[], _],_) :- !.
-s([Casa1, Casa2],[Casa1_Nueva, [X|Casa2]]) :-
+s([[], [X], Y],[[],[],[X|Y]]) :- !.
+
+s([Casa1, [], Casa2], [Casa1_Nueva, [X], Casa2]) :-
     member(X, Casa1),
     mover(X, Casa1, Casa1_Nueva),
-    not(estado_invalido(Casa1_Nueva, [X|Casa2])).
+    not(estado_invalido(Casa1_Nueva, Casa2)).
 
-meta([[], [perro, gato, hamster]]).
+s([Casa1, [X], Casa2], [Casa1, [], [X|Casa2]]) :-
+    not(estado_invalido(Casa1,[X|Casa2])).
+
+s([Casa1, [], Casa2], [Casa1, [X], Casa2_Nueva]) :-
+    member(X, Casa2),
+    mover(X, Casa2, Casa2_Nueva),
+    not(estado_invalido(Casa1, Casa2_Nueva)).
+
+s([Casa1, [X], Casa2], [[X|Casa1], [], Casa2]) :-
+    not(estado_invalido([X|Casa1],Casa2)).
+
+
+s([Casa1, [X], Casa2],[Casa1, [Y], [X|Casa2_Nueva]]) :-
+    member(Y, Casa2),
+    mover(Y, Casa2, Casa2_Nueva),
+    not(estado_invalido(Casa1,[X|Casa2_Nueva])).
+
+s([Casa1, [X], Casa2],[[X|Casa1_Nueva], [Y], Casa2]) :-
+    member(Y, Casa1),
+    mover(Y, Casa1, Casa1_Nueva),
+    not(estado_invalido([X|Casa1_Nueva],Casa2)).
+
+meta([[],[],P]) :-
+    permutation([perro,gato,hamster],P).
 
 mover(X, [X|Tail], Tail).
 mover(X, [Y|Tail], [Y|NewTail]) :-
@@ -22,6 +46,3 @@ estado_invalido_aux([Top|Tail]) :-
     (odia(Top, Other); odia(Other, Top)),
     member(Other, Tail),
     !.
-
-
-% mover([Top|Tail], R) :-
